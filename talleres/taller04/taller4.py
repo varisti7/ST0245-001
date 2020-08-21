92df79eafbd0ea74b987c136f431a24e3462c9d5
VAO <varisti6@gmail.com>
2020-08-20 22:34:18 -0500

    Taller 4 Vicente Aristizabal, entrega 1

--- talleres/taller04/taller4.py
+++ talleres/taller04/taller4.py
@@ -1,25 +1,18 @@
 import random
-from matplotlib import pyplot as pl
+import matplotlib.pyplot as plt
 import time
+import sys
 
-# def array_generator(len):
-#     """List generator"""
-   
+sys.setrecursionlimit(1000000000)
 
-# def array_sum(array, sum = 0):
-#     """Add the elements in the list"""
+def array_generator(len):
+    """List generator"""
+    choices = list(range(len))
+    array = []
+    for x in range(len):
+        array.append(random.choice(choices))
+    return array
  
-
-def multiplication_tables(n):
-    for i in range(1,n + 1):
-        for j in range(1,n + 1):
-            print (str(i) + " * " + str(j) + " = " + str(i*j))
-        print ("--------------------")
-
-
-# def insertion_sort(list):
-  
-
 def arrayMax(arr):
     return arrayMax_aux(arr, 0, 0)
 
@@ -32,15 +25,40 @@ def arrayMax_aux(arr, i, max):
         max = arrayMax_aux(arr, i + 1, max)
         return max
 
-# def groupSum_aux(list, start, target):
-  
+def groupSum_aux(start, lista, target):
+    if start == len(lista):
+        return target == 0
+    return groupSum_aux(start + 1, lista, target - lista[start]) or groupSum_aux(start + 1, lista, target)
 
-# def groupSum(list, target):
-   
+def groupSum(lista, target):
+   return groupSum_aux(0, lista, target)
 
 # #----------------------------Fibonacci---------------------------------#
 
 # def fib_r(n):                             #Fibonacci recursivo
 
-lista = [102,4,5,98,3,45,101]
-print(arrayMax(lista))
+lengthArray = []
+timeArray = []
+for x in range (1,22):
+    arr = array_generator(x)
+    num = random.choice(range(150))
+    lengthArray.append(x)
+    t1 = time.time()
+    print(groupSum_aux(0, arr, num))
+    t2 = time.time()
+    timeArray.append(t2 - t1)
+plt.plot(lengthArray, timeArray)
+plt.show()
+
+lengthArray = []
+timeArray = []
+for x in range (100,1101,50):
+    arr = array_generator(x)
+    lengthArray.append(len(arr))
+    t1 = time.time()
+    print(arrayMax_aux(arr, 0, 0))
+    t2 = time.time()
+    timeArray.append(t2 - t1)
+plt.plot(lengthArray, timeArray)
+plt.show()
+
